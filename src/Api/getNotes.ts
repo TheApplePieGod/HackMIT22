88f9016720fd672cd2b8ed75bbc65c4fcf778f45
Leo Gagnon <leognon@gmail.com>
2022-10-01 13:38:31 -0400

    Add db connection

---
+++ src/Api/getNotes.ts
@@ -0,0 +1,11 @@
+﻿import clientPromise from '../../lib/mongodb';
+
+export async function getNotes() {
+    const client = await clientPromise;
+    const db = client.db('prod');
+    const notes = await db.collection('notes').find({}).toArray();
+
+    return {
+      notes
+    }
+}
