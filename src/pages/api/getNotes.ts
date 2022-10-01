9e6bf87da0fbb9aa0549eebf747c075a355a0586
Leo Gagnon <leognon@gmail.com>
2022-10-01 14:53:09 -0400

    Fix up API routes

---
+++ src/pages/api/getNotes.ts
@@ -0,0 +1,11 @@
+﻿import clientPromise from 'lib/mongodb';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async (_: NextApiRequest, res: NextApiResponse) => {
+    const client = await clientPromise;
+    const db = client.db('prod');
+
+    const notes = await db.collection('notes').find({}).toArray();
+
+    res.status(200).json(notes);
+}
---
+++ types/note.d.ts
@@ -0,0 +1,6 @@
+interface Note {
+    id: string,
+    title: string,
+    img: string,
+    children: [string]
+}
