57500cf8990416565e4cac7b7cb842f3bb2bafcc
Leo Gagnon <leognon@gmail.com>
2022-10-01 18:53:53 -0400

    Update def

--- src/Definitions/note.d.ts
+++ src/Definitions/note.d.ts
@@ -1,8 +1,15 @@
+interface Manim {
+    pos: [number, number],
+    dim: [number, number],
+    img: string
+}
+
 interface Note {
     _id: string,
     title: string,
     author: string,
     img: string,
+    dim: [number, number], //width, height in pixels
     class: string,
     score: number,
     children: string[]
